a79de10228975257a5702411aef8e15362a51a63
Aearsears <53102185+Aearsears@users.noreply.github.com>
2022-05-05 00:05:48 -0400

    change path

--- Questgen/main.py
+++ Questgen/main.py
@@ -35,8 +35,8 @@ class QGen:
     def __init__(self):
         
         
-        self.tokenizer = T5Tokenizer.from_pretrained('t5-base')
-        model = T5ForConditionalGeneration.from_pretrained('Parth/result')
+        self.tokenizer = T5Tokenizer.from_pretrained(os.path.abspath("/app/t5"))
+        model = T5ForConditionalGeneration.from_pretrained(os.path.abspath("/app/t5_condi"))
         device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         model.to(device)
         # model.eval()
